88801e292ec21c44caff43540f41304cb1f59d3f
eseminara <eze.seminara@gmail.com>
2025-02-06 20:43:00 -0300

    fix candidate model for role

--- backend/core/candidato/models.py
+++ backend/core/candidato/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from core.usuario.models import CustomUser  # Importa desde la ruta completa
+from core.usuario.models import CustomUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -9,8 +11,15 @@ class Candidato(models.Model):
     educacion = models.TextField()
     cv = models.FileField(upload_to='cvs/', null=True, blank=True)
     
+    
+    
     def __str__(self):
         return self.experiencia
     
     class Meta:
         db_table = "candidato"
+        
+@receiver(post_save, sender=CustomUser)
+def create_recruiter_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'candidate':
+        Candidato.objects.create(user=instance)
