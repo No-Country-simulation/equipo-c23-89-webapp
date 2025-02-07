26623d6dd8f2611346ef7279dc1d4457053264f3
Raúl Quimbaya <raulquimbaya4@gmail.com>
2025-02-06 22:19:05 -0500

    refactor: update candidate and offer types to use User interface

--- frontend/src/recruiter/store/offer.ts
+++ frontend/src/recruiter/store/offer.ts
@@ -4,10 +4,14 @@ import { type Offer } from '../types/offer'
 
 interface OfferStore {
   offers: Offer[]
+  allOffers: Offer[]
   setOffers: (offer: Offer[]) => void
+  setAllOffers: (offer: Offer[]) => void
 }
 
 export const useOfferStore = create<OfferStore>((set) => ({
   offers: [OFFER_INITIAL_VALUES],
-  setOffers: (offers: Offer[]) => { set({ offers }) }
+  allOffers: [OFFER_INITIAL_VALUES],
+  setOffers: (offers: Offer[]) => { set({ offers }) },
+  setAllOffers: (allOffers: Offer[]) => { set({ allOffers }) }
 }))
